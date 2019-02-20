2819dc2f60dfbfd4de35fba596f0fc1b5f6848b7
jsouthwick01 <jsouthwick01@users.noreply.github.com>
2019-02-20 12:10:46 -0700

    Added basic scene management
    Created a new manager scene (May be wrong) and set it as default. Added a script to deal with switching between scenes. This is a rough sketch, it will definitely need to be revised once we have added in scenes. Perhaps I should save this to the master, but I don't want to break anything - Jacob

---
+++ Level One Project/Assets/Scripts/LevelController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Creates an enum for each level scene
+public enum Levels { MainMenu, Level1, Level2, Level3}
+public class LevelController : MonoBehaviour
+{
+    private GameObject player;
+    //This enum will be used to track the level it should be
+    public Levels levels;
+    private Levels currentLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Gets a reference to the player and sets the levels enum
+        player = GameObject.FindGameObjectWithTag("player");
+        levels = Levels.MainMenu;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //This will make sure that the scene is loaded only once.
+        if(levels != currentLevel)
+        {
+            //Checks which level it needs to be
+            switch (levels)
+            {
+                case Levels.MainMenu:
+                    {
+                        //Loads scene and sets the current level so it is correct.
+                        SceneManager.LoadScene("MainMenu", LoadSceneMode.Additive);
+                        currentLevel = levels;
+                        break;
+                    }
+                case Levels.Level1:
+                    {
+                        SceneManager.LoadScene("Level1", LoadSceneMode.Additive);
+                        currentLevel = levels;
+                        //This will reactivate the player, which was deactivated for the main menu
+                        player.SetActive(true);
+                        break;
+                    }
+                case Levels.Level2:
+                    {
+                        SceneManager.LoadScene("Level2", LoadSceneMode.Additive);
+                        currentLevel = levels;
+                        break;
+                    }
+                case Levels.Level3:
+                    {
+                        SceneManager.LoadScene("Level3", LoadSceneMode.Additive);
+                        currentLevel = levels;
+                        break;
+                    }
+            }
+        }
+       
+    }
+}
--- Level One Project/Assets/Scripts/PlayerController.cs
+++ Level One Project/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public enum States { NavMesh, WASD, MeleeAttack, RangeAttack, Neutral };
 
